facac707ddc163c0d9adca65e205799c791cb101
statulr <122219240+statulr@users.noreply.github.com>
2023-11-30 21:21:56 -0500

    Refactor validActions array and create a hash set for efficient action validation

--- src/aurc_valid.c
+++ src/aurc_valid.c
@@ -3,6 +3,30 @@
 #include <string.h>
 #include <search.h>
 
+// Define valid actions
+char *validActions[] = {
+    "install", "install-force", "remove", "query", "search", "remove-dep",
+    "clear-aur-cache", "update", "refresh", "modify-repo", "remove-force",
+    "remove-force-dep", "search-aur", "install-aur", "install-local", "remove-orp",
+    "github", "config"};
+
+// Calculate the size of validActions array dynamically
+size_t numValidActions = sizeof(validActions) / sizeof(validActions[0]);
+
+// Create a hash set for valid actions
+void __attribute__((constructor)) createHashSet()
+{
+    hcreate(numValidActions);
+    ENTRY e;
+
+    for (size_t i = 0; i < numValidActions; i++)
+    {
+        e.key = validActions[i];
+        e.data = (void *)0;
+        hsearch(e, ENTER);
+    }
+}
+
 // Function to check if an action is valid
 int isValidAction(const char *action)
 {
@@ -12,31 +36,10 @@ int isValidAction(const char *action)
         return 0;
     }
 
-    // Define valid actions
-    char *validActions[] = {
-        "install", "install-force", "remove", "query", "search", "remove-dep",
-        "clear-aur-cache", "update", "refresh", "modify-repo", "remove-force",
-        "remove-force-dep", "search-aur", "install-aur", "install-local", "remove-orp",
-        "github", "config"};
-
-    // Calculate the size of validActions array dynamically
-    size_t numValidActions = sizeof(validActions) / sizeof(validActions[0]);
-
-    // Create a hash set for valid actions
-    hcreate(numValidActions);
-    ENTRY e, *ep;
-
-    for (size_t i = 0; i < numValidActions; i++)
-    {
-        e.key = validActions[i];
-        e.data = (void *)0;
-        hsearch(e, ENTER);
-    }
-
     // Main logic
+    ENTRY e, *ep;
     e.key = (char *)action;
     ep = hsearch(e, FIND);
-    hdestroy(); // Destroy the hash set
 
     return ep != NULL;
 }
